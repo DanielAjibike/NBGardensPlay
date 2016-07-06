ec1a835c4be022e6977e67e5fcc4354772c76278
Nick <Nickkaraolis@yahoo.com>
2016-07-06 17:10:11 +0100

    Added registration form, customer details and registration controller

--- NBGardenWebsite/app/controllers/RegistrationController.scala
+++ NBGardenWebsite/app/controllers/RegistrationController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
-import play.api.mvc.{Action, Controller, Flash}
+import play.api.mvc.{Action, Controller, Flash, Request}
 
 /**
   * Created by Administrator on 06/07/2016.
