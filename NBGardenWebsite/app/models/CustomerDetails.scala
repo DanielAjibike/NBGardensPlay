e138541fa958d1836cb4772ed3c979e1aa216bd5
Nick <Nickkaraolis@yahoo.com>
2016-07-08 09:53:14 +0100

    Added registration file, routes and messages

--- NBGardenWebsite/app/models/CustomerDetails.scala
+++ NBGardenWebsite/app/models/CustomerDetails.scala
@@ -6,18 +6,13 @@ import play.api.data._
 /**
   * Created by Administrator on 06/07/2016.
   */
-case class CustomerDetails(firstName: String, lastName: String, email: String, telephone: Int, username: String, password: String) {
+case class CustomerDetails(firstName: String, lastName: String, email: String, telephone: Int, username: String, password: String)
 
-  val userForm = Form(
-    mapping(
-      "name" -> text,
-      "lastName" -> text,
-      "email" -> text,
-      "telephone" -> number,
-      "username" -> text,
-      "password" -> text
-    )(CustomerDetails.apply)(CustomerDetails.unapply)
-  )
+  object Customer {
+    var customers = Set(CustomerDetails("Nick", "Noob", "MrNoob@everywhere.com", 999, "bigNoob", "mrPassword"))
 
+    def add(customer : CustomerDetails): Unit ={
+      customers = customers + customer
+    }
+  }
 
-}
