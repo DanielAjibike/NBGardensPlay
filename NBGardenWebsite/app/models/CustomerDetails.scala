06a3e6db5d7d932cd6911bb990b12f89e908e835
Nick <Nickkaraolis@yahoo.com>
2016-07-08 09:48:47 +0100

    Registration form created using Play form

--- NBGardenWebsite/app/models/CustomerDetails.scala
+++ NBGardenWebsite/app/models/CustomerDetails.scala
@@ -1,14 +1,12 @@
-package entity
+package models
 
-import play.api.data._
 import play.api.data.Forms._
+import play.api.data._
 
 /**
   * Created by Administrator on 06/07/2016.
   */
 case class CustomerDetails(firstName: String, lastName: String, email: String, telephone: Int, username: String, password: String) {
-
-
 
   val userForm = Form(
     mapping(
